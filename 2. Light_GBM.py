3bc63a7c6c30074fdab2f74d7c9c10f37b12191f
sarahsoyeonoh <110349903+sarahsoyeonoh@users.noreply.github.com>
2022-09-26 20:57:41 -0400

    Add files via upload

--- 2. Light_GBM.py
+++ 2. Light_GBM.py
@@ -1,29 +1,28 @@
-#IMPORT PACKAGES
-import os
+from multiprocessing.sharedctypes import Value
+from pathlib import Path
 import pandas as pd
-import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.inspection import permutation_importance
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn import metrics
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
+import lightgbm as lgb
+import numpy as np
 
 data = pd.read_csv('/Users/sarahoh/Desktop/py_scripts/hello/220901/dataset.csv')
 
-#FIT LOGISTIC REGRESSION MODEL
-X = data[['ethnicity', 'race','maternal_age','complications', 'type', 'prenatal_care', 'first_frequency', 'first_amount', 'second_frequency', 'second_amount', 'third_frequency', 'third_amount']]
+X = data[['ethnicity', 'race','maternal_age','complications', 'type', 'prenatal_care',
+ 'first_frequency', 'first_amount', 'second_frequency', 'second_amount', 'third_frequency', 'third_amount']]
 y = data['fas_f']
-
-#split the dataset into training (70%) and testing (30%) sets
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=0)
 
-#instantiate the model
-log_regression = LogisticRegression(max_iter=100)
-#fit the model using the training data
-log_regression.fit(X_train,y_train)
-
-#use model to make predictions on test data
-y_pred = log_regression.predict(X_test)
+# build the lightgbm model
+import lightgbm as lgb
+clf = lgb.LGBMClassifier()
+clf.fit(X_train, y_train)
+y_pred=clf.predict(X_test)
 
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 print("Precision:", metrics.precision_score(y_test, y_pred))
@@ -38,10 +37,9 @@ sensitivity1 = cm1[0,0]/(cm1[0,0]+cm1[0,1])
 print('Sensitivity : ', sensitivity1 )
 specificity1 = cm1[1,1]/(cm1[1,0]+cm1[1,1])
 print('Specificity : ', specificity1)
-pos_pred_val = cm1[0,0]/(cm1[0,0]+cm1[0,1])
 
 #plot ROC curve
-y_pred_proba = log_regression.predict_proba(X_test)[::,1]
+y_pred_proba = clf.predict_proba(X_test)[::,1]
 fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba)
 auc = metrics.roc_auc_score(y_test, y_pred_proba)
 plt.plot(fpr,tpr,label="AUC="+str(auc))
